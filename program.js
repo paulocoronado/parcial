9475a916cb5c00ee75602d368f7510ec0b82066a
Paulo Coronado <paulocoronado@gmail.com>
2025-02-28 19:27:31 -0500

    v 0.3

--- program.js
+++ program.js
@@ -4,3 +4,18 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+async function loadPolygon(){
+    let myData = await fetch('britalia.geojson');
+    let myPolygon = await myData.json();
+
+    L.geoJSON(myPolygon,
+        {
+            style:{
+                color:'blue'
+            }
+        }
+    ).addTo(map);
+}
+
+loadPolygon();
