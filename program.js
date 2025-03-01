3fb14d2498a6d74e6cd8035f3c32bd1becc414d6
Paulo Coronado <paulocoronado@gmail.com>
2025-03-01 09:59:24 -0500

    V 0.3

--- program.js
+++ program.js
@@ -19,3 +19,79 @@ async function loadPolygon(){
 }
 
 loadPolygon();
+
+let btnTrees= document.getElementById("btnTrees");
+
+btnTrees.addEventListener('click',
+    async ()=>{
+        let response= await fetch("arboles_britalia.geojson");
+        let datos= await response.json();
+
+        //Agregar la capa al mapa
+        L.geoJSON(
+            datos,
+            {
+                pointToLayer: (feature, latlong)=>{                    
+                    return L.circleMarker(latlong,{
+                        radius:5,
+                        fillColor:'green',
+                        weight:1,
+                        opacity:1,
+                        fillOpacity: 0.5,
+                    })
+                }
+            }
+        ).addTo(map);
+    }
+)
+
+let btnDistance= document.getElementById("btnDistance");
+
+btnDistance.addEventListener('click',
+    async ()=>{
+        let response= await fetch("arboles_britalia.geojson");
+        let datos= await response.json();
+        let trees= datos.features.map((myElement, index)=>({
+            id: index+1,
+            coordinates: myElement.geometry.coordinates
+        }));        
+
+        let distances=[];
+        trees.forEach( (treeA)=>{trees.forEach(
+
+            
+                (treeB)=>{
+                    if(treeA.id != treeB.id){
+                        let distance = turf.distance( 
+                            turf.point(treeA.coordinates),
+                            turf.point(treeB.coordinates),
+                        );
+                        distances.push(
+                            [
+                                `Árbol ${treeA.id}`,
+                                `Árbol ${treeB.id}`,
+                                distance.toFixed(3)                            
+                            ]
+                        )
+                }
+            }
+            )
+        }
+        )
+        generatePDF(distances, trees.lenght);
+    }
+)
+function generatePDF(distances, totalTrees){
+    let { jsPDF } = window.jspdf;
+    let documentPDF= new jsPDF();   
+    
+    documentPDF.text("REPORTE DE ÁRBOLES EN EL BARRIO GRAN BRITALIA", 10,10);
+
+    documentPDF.autoTable(
+        {
+            head: [['Árbol 1', 'Árbol 2', 'Distance']],
+            body: distances
+        }
+    );
+    documentPDF.save("britalia.pdf")
+}
